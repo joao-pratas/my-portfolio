046b1c19b6f7e58256a0d80a17e56b966ca7bcd9
Joao Pratas <joao.pratas@ADYTT0HHFVH4.local>
2018-02-09 11:41:22 +0000

    initial reakit setup and started molding the main menu

---
+++ joao-pratas/src/features/home/DefaultPage.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actions from './redux/actions';
+
+export class DefaultPage extends Component {
+  static propTypes = {
+    home: PropTypes.object.isRequired,
+    actions: PropTypes.object.isRequired,
+  };
+
+  render() {
+    return (
+      <div className="home-default-page">
+         <div id="main-menu">
+          <div id="logo"></div>
+          <div class="menu-items">
+            <ul>
+              <li>HOME</li>
+            </ul>
+          </div>
+         </div>
+      </div>
+    );
+  }
+}
+
+/* istanbul ignore next */
+function mapStateToProps(state) {
+  return {
+    home: state.home,
+  };
+}
+
+/* istanbul ignore next */
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators({ ...actions }, dispatch)
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DefaultPage);
